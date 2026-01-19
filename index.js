3e678e7fece71b15b6b09b524e23c0cba5a999be
chenhaoyue <chenhaoyue1@huawei.com>
2026-01-19 17:50:46 +0800

    add

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import App1 from './tests/react-native-screens/index.tsx'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App1);
