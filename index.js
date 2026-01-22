c5682c6a66756fe23c16ee4537f1133a35bd094b
chenhaoyue <chenhaoyue1@huawei.com>
2026-01-22 17:01:39 +0800

    add react-native-gesture-handler,react-native-reanimated,react-native-safe-area-context

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import App1 from './tests/react-native-screens/index.tsx'
+// import App1 from './tests/react-native-screens/index.tsx'
+import App1 from './tests/260116160802092/react-native-clipboard/index.tsx'
 
 AppRegistry.registerComponent(appName, () => App1);
